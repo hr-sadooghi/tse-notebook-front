6292643319682362c0fd4c08aa2a7c47d4630520
Amiri267 <m.amiri267@gmail.com>
2019-06-16 21:35:30 +0430

    add search item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './router/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FilterListPipe } from './core/pip/filter-list.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterListPipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/core/pip/filter-list.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filterList'
+})
+export class FilterListPipe implements PipeTransform {
+
+
+  transform(value: Array<any> , args?: string): any {
+    const pattern = new RegExp(args.toString());
+    if (!args) {
+      return value;
+    } else {
+      return value.filter((item) => {
+        return pattern.test(item.title);
+      });
+    }
+  }
+
+
+
+}
